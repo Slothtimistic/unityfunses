be264d3822d64758bf0f22c86b5fcb9158c2faa9
Maverique <Marcusbarone0922@gmail.com>
2015-09-18 20:28:33 -0400

    cubespawn

---
+++ cubeSpawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeSpawn : MonoBehaviour {
+
+	GameObject enemy;
+	float RNGesus;
+	float RNGesus2;
+	// Use this for initialization
+	void Start () {
+		RNGesus = Random.value;
+		RNGesus2 = Random.value;
+		if (RNGesus > 0.5f) {
+			enemy = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		} else {
+			enemy = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		}
+		if (RNGesus2 > 0.5f) {
+			enemy.transform.position = new Vector3 (-2.0f, 0.25f, 0);
+		} else {
+			enemy.transform.position = new Vector3 (2.0f, -0.25f, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
